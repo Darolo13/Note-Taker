cdc6c75a7b123b8a54b7073ad0542b417a0bba84
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-08-28 15:04:19 -0700

    Add apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,46 +1,37 @@
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
+let { noteData } = require('../../db/db.json');
 
-let noteCount = 1;
 
-const readData = () => {
-    const noteData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')))
-    return noteData;
+router.get('/notes', (req, res) => {
+    let newNotes = noteData;
+    res.json(newNotes)
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = noteData.length.toString();
+    const note = createNote(req.body, noteData);
+    res.json(noteData);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let noteData = readData();
+    const noteId = req.params.id;
+    const newNoteData = noteData.filter((note) => note.id != noteId);
+    
+    writeData(newNoteData);
+    res.send(newNoteData);
+});
+
+function createNote(body, noteD) {
+    const note = body;
+    noteD.push(note);
+
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ noteData:noteD }, null, 2)
+    );
+    return note;
 }
-
-const writeData = (noteData) => {
-    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(noteData), (err) => {
-        if (err) return ({ err });
-    });
-}
-
-const newNoteId = () => noteId++;
-
-    router.get('/notes', (req, res) => {
-        let noteData = readData();
-        res.json(noteData)
-    })
-
-    router.post('/notes', (req, res) => {
-        let noteData = readData();
-        let newNote = req.body;
-        let lastNoteId = !noteData[0] ? 0 : noteData[noteData.length - 1].id;
-        let newNoteId = lastNoteId + 1;
-
-        newNote.Id = newNoteId;
-        noteData.push(newNote);
-        writeData(noteData);
-        return res.json(noteData)
-    })
-
-    router.delete('/notes/:id', (req, res) => {
-        let noteData = readData();
-        const noteId = req.params.id;
-        const newNoteData = noteData.filter((note) => note.id != noteId);
-
-        writeData(newNoteData);
-        res.send(newNoteData);
-    })
-
-    module.exports = router;
+module.exports = router;
